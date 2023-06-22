ed2908b37f5a24ddaa6578bcec39c9edfb2e09b2
Kees Broersen <kbroersen@sqli.com>
2023-06-22 14:29:47 +0200

    fix/ts error

--- src/components/SelectPhone.vue
+++ src/components/SelectPhone.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Kies uw telefoon</h1>
     <select v-model="ppi" @change="onChange">
-      <option value="" disabled selected hidden>Kies je telefoon</option>
+      <option :value="0" disabled selected hidden>Kies je telefoon</option>
       <option v-for="phone in phones" :key="phone.title" :value="phone.ppi">{{ phone.title }}</option>
     </select>
 
@@ -14,9 +14,14 @@
 import { ref } from 'vue';
 import devices from './devices.json';
 
+interface phone {
+  title: string;
+  ppi: number;
+}
+
 const emit = defineEmits(['pixelsPerMm','setPage']);
-const ppi = ref("");
-const phones = devices.phones;
+const ppi = ref(0);
+const phones = devices.phones as phone[];
 
 const onChange = () => {
   const pixelsPerMm = ppi.value / 25.4;
@@ -32,14 +37,14 @@ const onChange = () => {
 }
 
 select {
-  margin: 32px 0;
-  min-height: 40px;
+  margin: 32px 0 16px;
+  min-height: 50px;
   -webkit-appearance: none;
   background: #eee;
   width: 100%;
   border: none;
   border-radius: 5px;
-  padding: 8px;
+  padding: 8px 16px;
   font-size: 1em;
 }
 </style>
